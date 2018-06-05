082300f1ba9e03509801f9e2ee299a1625634d5d
n0psledbyte <n0psledbyte@gmail.com>
2018-06-05 21:00:50 +0700

    remove unused dir and comment

--- app/index.js
+++ app/index.js
@@ -5,21 +5,8 @@ import { Provider } from 'react-redux';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './redux-test/css/itemslist.css';
 
-//import App from './App.jsx';
-//import App from './State.jsx';
-//import App from './GantiWarna.jsx';
-//import App from './Router.jsx';
-//import App from './flux/components/App';
-//import App from './belajar-bootstrap/App';
-
-//ReactDOM.render(<App />, document.getElementById('app'));
-//setTimeout(() => {
-//    ReactDOM.unmountComponentAtNode(document.getElementById('app'))
-//}, 10000)
-
-//belajar redux --[
-import App from './redux-test/components/App';
-import store from './redux-test/stores/store';
+import App from './components/App';
+import store from './stores/store';
 
 ReactDOM.render(
     <Provider store={store}>
@@ -27,4 +14,3 @@ ReactDOM.render(
     </Provider>,
     document.getElementById('app')
 )
-//--]
